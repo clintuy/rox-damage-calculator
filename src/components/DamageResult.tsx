05014793b28e08e4cbfef8193d9dc46d462f7810
clintuy <benavidez.clint@gmail.com>
2026-05-05 19:58:07 +0800

    Fix damage label color

--- src/components/DamageResult.tsx
+++ src/components/DamageResult.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function DamageResult({ finalDmg = 0 }: Props) {
   return (
-    <h1 className="font-bold p-5 text-lg sm:text-xl md:text-2xl">
+    <h1 className="font-bold p-5 text-lg sm:text-xl md:text-2xl text-[#fa774a]">
       Final Damage: {formatDamage(finalDmg)}
     </h1>
   );
